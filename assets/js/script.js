0ca7142df75ca985e31cf17e3f7ddc32fca471b8
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-06-10 16:56:41 -0400

    Added eventlisteners to each answer button and the form. Added new functions such as results, submit score, etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
 //variables connecting the Javascript to parts of the HTML
 
 var startQuiz = document.querySelector("#start-quiz");
+var titleCard = document.querySelector("#title-card");
+var questionCard = document.querySelector("#question-card");
+var resultsCard = document.querySelector("#results-card");
+var highScoresCard = document.querySelector("#highScoresCard");
+var highScoresForm = document.querySelector("#highScoresForm");
 var question = document.querySelector("#question");
-var nextQuestion = document.querySelector('.button');
+var ansButton = document.querySelector(".ansButton");
 var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
 var timerEl = document.querySelector("#timer");
-var corIncor = document.querySelector("#corIncor")
+var corIncor = document.querySelector("#corIncor");
+var finalScore = document.querySelector("#final-score");
+var scoreCurrent = document.querySelector("#score");
+var highScoreList = document.querySelector("#highScoreList");
 
 //Variables needed to keep track of data
-var score = 0;
+var score = localStorage.getItem("score");
+scoreCurrent.textContent = score + " points";
 var timeLeft = 75;
+score = 0;
 
 //An array of objects, each object being a different question and answer
 var questionBank = [{
@@ -42,59 +52,149 @@ var questionBank = [{
 }
 ];
 
+// var playerStats = {
+//     initials: initials.value,
+//     score: finalScore
+// }
 
-startQuiz.addEventListener("click", function(){
-    countdown();
-    document.querySelector("#title-card").setAttribute("data-state", "hidden");
-    document.querySelector("#question-card").setAttribute("data-state", "visible");
+var players = [];
 
-    // questionAnswers(); 
+startQuiz.addEventListener("click", function(event){
+    event.preventDefault();
+    init();
     var q = 0;
+    
+    titleCard.setAttribute("data-state", "hidden");
+    questionCard.setAttribute("data-state", "visible");
 
-    function corIncorClear(){
-        corIncor.textContent ="";
-    }
+    corIncorClear();
 
     function quesAnsText () {
-        question.textContent = questionBank[q].question;
-        answer1.textContent = questionBank[q].answers[0];
-        answer2.textContent = questionBank[q].answers[1];
-        answer3.textContent = questionBank[q].answers[2];
-        answer4.textContent = questionBank[q].answers[3];
-    }
+        if (q < questionBank.length) {
+            question.textContent = questionBank[q].question;
+            answer1.textContent = questionBank[q].answers[0];
+            answer2.textContent = questionBank[q].answers[1];
+            answer3.textContent = questionBank[q].answers[2];
+            answer4.textContent = questionBank[q].answers[3];
+        } else {
+            setTimeout(results,1000);
+            return;
+        }
+    };
+
+    answer1.addEventListener("click", function(event){
+        event.preventDefault();
+        if (q < questionBank.length) {
+            if ((answer1.textContent === questionBank[q].answer) && (timeLeft !== 0)) {
+                corIncor.textContent = "Correct!";
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score + 10;
+                scoreCurrent.textContent = score + " points";
+                quesAnsText();
+            } else {
+                corIncor.textContent = "Incorrect!"
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score - 5;
+                scoreCurrent.textContent = score + " points";
+                timeLeft = timeLeft - 15;
+                quesAnsText();
+            } 
+        } else {
+            console.log("yeeepers");
+            setTimeout(results,1000);
+            return;
+        }
+    
+    });
+
+    answer2.addEventListener("click", function(){
+        if (q < questionBank.length) {
+            if ((answer2.textContent === questionBank[q].answer) && (timeLeft !== 0)) {
+                corIncor.textContent = "Correct!";
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score + 10;
+                scoreCurrent.textContent = score + " points";
+                quesAnsText();
+            } else {
+                corIncor.textContent = "Incorrect!"
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score - 5;
+                scoreCurrent.textContent = score + " points";
+                timeLeft = timeLeft - 15;
+                quesAnsText();
+            } 
+        } else {
+            console.log("yeeepers");
+            setTimeout(results,1000);
+            return;
+        }
+    
+    });
+
+    answer3.addEventListener("click", function(){
+        if (q < questionBank.length) {
+            if ((answer3.textContent === questionBank[q].answer) && (timeLeft !== 0)) {
+                corIncor.textContent = "Correct!";
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score + 10;
+                scoreCurrent.textContent = score + " points";
+                quesAnsText();
+            } else {
+                corIncor.textContent = "Incorrect!"
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score - 5;
+                scoreCurrent.textContent = score + " points";
+                timeLeft = timeLeft - 15;
+                quesAnsText();
+            } 
+        } else {
+            console.log("yeeepers");
+            setTimeout(results,1000);
+            return;
+        }
+    
+    });
+
+    answer4.addEventListener("click", function(){
+        if (q < questionBank.length) {
+            if ((answer4.textContent === questionBank[q].answer) && (timeLeft !== 0)) {
+                corIncor.textContent = "Correct!";
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score + 10;
+                scoreCurrent.textContent = score + " points";
+                quesAnsText();
+            } else {
+                corIncor.textContent = "Incorrect!"
+                setTimeout(corIncorClear,1500);
+                q++;
+                score = score - 5;
+                scoreCurrent.textContent = score + " points";
+                timeLeft = timeLeft - 15;
+                quesAnsText();
+            } 
+        } else {
+            console.log("yeeepers");
+            setTimeout(results,1000);
+            return;
+        }
+    
+    });
 
     quesAnsText();
-
-    answer1.addEventListener("click", function(){
-        if ((answer1.textContent === questionBank[q].answer) && (timeLeft !== 0)) {
-            console.log("Correct!");
-            corIncor.textContent = "Correct";
-            setTimeout(corIncorClear,1000);
-            q++;
-            score = score + 10;
-            quesAnsText();
-        } else {
-            console.log("Incorrect!");
-            corIncor.textContent = "Incorrect!"
-            q++;
-            score = score - 5;
-            timeLeft = timeLeft - 15;
-            quesAnsText();
-        } 
-
-        if (q === questionBank.length) {
-            document.querySelector("#question-card").setAttribute("data-state", "hidden");
-        }
-    });
+    countdown();
 
 });
 
-
-
 //Timercountdown (Starting from 75 seconds)
 function countdown() {
-    // var timeLeft = 75;
-  
+
     var timeInterval = setInterval(function () {
       if (timeLeft > 1) {
         timerEl.textContent = timeLeft + ' seconds';
@@ -105,22 +205,52 @@ function countdown() {
       } else {
         timerEl.textContent = '';
         clearInterval(timeInterval);
-        results();
+        questionCard.setAttribute("data-state", "hidden");
+        resultsCard.setAttribute("data-state", "visible");
       }
     }, 1000);
   }
 
-//   function questionAnswers(){
-//     question.textContent = questionBank[0].question;
-//     answer1.textContent = questionBank[0].answers[0];
-//     answer2.textContent = questionBank[0].answers[1];
-//     answer3.textContent = questionBank[0].answers[2];
-//     answer4.textContent = questionBank[0].answers[3];
-//   }
+function init() {
+    score = 0;
+    scoreCurrent.textContent = score + " points";
+};
 
+function submitScore(event){
+    event.preventDefault();
+    console.log("submit score's logged?")
+    for (var i = 0; i < players.length; i++) {
+        var player = {
+            initials: initials.value,
+            score: score
+        }
+        
+        player = players[i];
 
+        var li = document.createElement("li");
+        li.textContent = i +". " + player.initials + "  " + player.score;
+        li.setAttribute("data-index", i);
 
+        highScoreList.appendChild(li);
+    }
+}
 
+function corIncorClear(){
+    corIncor.textContent ="";
+};
+
+function results() {
+    questionCard.setAttribute("data-state", "hidden");
+    resultsCard.setAttribute("data-state", "visible");
+    highScoresCard.setAttribute("data-state", "visible");
+    scoreTime = timeLeft;
+    timeLeft = 0;
+    timerEl.textContent = '';
+    finalScore.textContent = "Your score: " + score;
+    localStorage.setItem("score", score);
+};
+
+highScoresForm.addEventListener("submit", submitScore);
 
 
 
@@ -131,41 +261,6 @@ function countdown() {
 //click event for the link to the high scores
 
 //High Score Function that hides all other parts of the HTML, and makes the high score visible
-
-
-
-
-
-
-
-
-
-// var timeEl = document.querySelector(".timer");
-// var startQuizButton = document.querySelector("#start-quiz");
-// var questionEl = document.querySelector(".box");
-// var state = box.getAttribute("data-state");
-// var box = document.querySelector(".box");
-
-// startQuizButton.addEventListener("click", startQuiz);
-
-// function startQuiz(){
-
-//     console.log("hello")
-
-//     var state = box.getAttribute("data-state");
-
-//     if (state === "visible") {
-//         state = "hidden";
-//     }
-
-//     if (state === "hidden") {
-//         state = "visible";
-//     }
-    
-    
-    // var element = event.target;
-
-    // var state = element.getAttribute("data-state");
 
 
 
